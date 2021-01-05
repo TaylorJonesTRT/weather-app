126cbb3be0f652c15a05b25a1e098da974c3a732
Taylor Jones <taylor@taylorwjones.com>
2021-01-04 19:22:53 -0600

    Completed the weather app by adding in units of measure switcher

--- src/index.js
+++ src/index.js
@@ -3,34 +3,45 @@ import DisplayController from './modules/DisplayController';
 
 const displayController = new DisplayController();
 const handleWeather = new HandleWeather();
-let units;
 
 const input = document.querySelector('.search-input');
 input.addEventListener('keyup', async (event) => {
   if (event.keyCode === 13) {
     event.preventDefault();
     const location = input.value;
-    const weatherImperial = await handleWeather.fetchWeather(location, 'imperial');
-    const weatherMetric = await handleWeather.fetchWeather(location, 'metric');
-    const temp = handleWeather.findTemp(weatherImperial);
-    const name = handleWeather.getName(weatherImperial);
-    const desc = handleWeather.findDesc(weatherImperial);
-    const feelsLike = handleWeather.findFeelsLike(weatherImperial);
-    const highLow = handleWeather.findMaxMinTemp(weatherImperial);
-    const humidity = handleWeather.findHumidity(weatherImperial);
+    const weather = await handleWeather.fetchWeather(location);
+    const temp = handleWeather.findTemp(weather);
+    const name = handleWeather.getName(weather);
+    const desc = handleWeather.findDesc(weather);
+    const feelsLike = handleWeather.findFeelsLike(weather);
+    const highLow = handleWeather.findMaxMinTemp(weather);
+    const humidity = handleWeather.findHumidity(weather);
     displayController.updateText(temp, name, desc, feelsLike, highLow, humidity);
     input.value = '';
   }
 });
 
 const fSpan = document.querySelector('.f-span');
-fSpan.addEventListener('click', () => {
+fSpan.addEventListener('click', async () => {
+  const location = document.querySelector('.location').textContent;
+  const weather = await handleWeather.fetchWeather(location, 'imperial');
+  const temp = handleWeather.findTemp(weather);
+  const name = handleWeather.getName(weather);
+  const desc = handleWeather.findDesc(weather);
+  const feelsLike = handleWeather.findFeelsLike(weather);
+  const highLow = handleWeather.findMaxMinTemp(weather);
+  const humidity = handleWeather.findHumidity(weather);
   displayController.updateText(temp, name, desc, feelsLike, highLow, humidity); 
 });
 const cSpan = document.querySelector('.c-span');
-cSpan.addEventListener('click', () => {
+cSpan.addEventListener('click', async () => {
+  const location = document.querySelector('.location').textContent;
+  const weather = await handleWeather.fetchWeather(location, 'metric');
+  const temp = handleWeather.findTemp(weather);
+  const name = handleWeather.getName(weather);
+  const desc = handleWeather.findDesc(weather);
+  const feelsLike = handleWeather.findFeelsLike(weather);
+  const highLow = handleWeather.findMaxMinTemp(weather);
+  const humidity = handleWeather.findHumidity(weather);
   displayController.updateText(temp, name, desc, feelsLike, highLow, humidity);
 });
-
-// TODO: Need to create a function that will handle whats in the event listener above and have the
-// TODO: listener just call the function so that the units can be changed even after a location is loaded.
--- src/modules/HandleWeather.js
+++ src/modules/HandleWeather.js
@@ -41,7 +41,7 @@ class HandleWeather {
   }
 
   // API Functions
-  async fetchWeather(location, units) {
+  async fetchWeather(location, units = 'imperial') {
     const locat = location;
     const measurementUnits = units;
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locat}&APPID=5714462c1533311f2fccfad1cc68c6b9&units=${measurementUnits}`, { mode: 'cors' });
